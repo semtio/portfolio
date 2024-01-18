4fd77ec287b382b471a8f04c52f59d4f65afd35e
semtio <semik20092@gmail.com>
2024-01-18 22:39:08 +0100

    v1.12

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ let summaryPUSH = document.querySelector('#summaryPUSH');
 let summaryText = document.querySelector('#summaryText');
 
 summaryPUSH.addEventListener('click', function () {
-    summaryText.style.opacity = '1'
+    summaryText.classList.toggle('animate-1');
 });
