02e18e6d30fafef53deb9498ad2cf69c3d1d09bf
semtio <semik20092@gmail.com>
2024-01-18 21:54:31 +0100

    v1.7

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let summary = document.querySelector('summary');
 let summaryText = document.querySelector('#summaryText');
 
 summaryText.style.opacity = '0';
+summaryText.style.color = 'lime';
 summaryText.style.marginTop = '-20px';
 
 summary.addEventListener('click', function () {
