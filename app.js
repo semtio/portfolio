a15c080ef4801393e289b95b9bad21c69ce01d43
semtio <semik20092@gmail.com>
2024-01-18 22:19:07 +0100

    v1.10

--- app.js
+++ app.js
@@ -1,21 +1,2 @@
 let summary = document.querySelector('#summary');
 let summaryText = document.querySelector('#summaryText');
-
-// summaryText.style.opacity = '0';
-// summaryText.style.display = 'none';
-// summaryText.style.color = 'lime';
-// summaryText.style.marginTop = '-20px';
-
-let counterSummary = 0;
-summary.addEventListener('click', function f() {
-    counterSummary++;
-    summaryText.classList.add('animate-1');
-    summaryText.style.display = 'block';
-
-    // if (counterSummary > 2) {
-    //     summaryText.style.opacity = '0';
-    //     summaryText.style.color = 'lime';
-    //     summaryText.style.marginTop = '-20px';
-    // }
-
-});
