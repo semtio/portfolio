e56533820e52bd0df44d79aa1078c3e2e291b3bf
semtio <semik20092@gmail.com>
2024-09-25 19:34:30 +0200

    reload all files

--- app.js
+++
@@ -1,7 +0,0 @@
-// let summaryPUSH = document.querySelector('#summaryPUSH');
-// let summaryText = document.querySelector('#summaryText');
-
-// summaryPUSH.addEventListener('click', function () {
-//     summaryText.classList.toggle('animate-1');
-//     summaryText.classList.toggle('animate-2');
-// });
